b6d0c06de8241b482895cd752bbc46584cd41db2
Camilo <camilo.riquelme@outlook.es>
2023-08-22 12:41:34 -0400

    Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const { response } = require("express");
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
-const { generarJWT } = require('../helpers/jwt')
+const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async ( req, res = response ) => {
 
@@ -43,6 +44,47 @@ const login = async ( req, res = response ) => {
     }
 }
 
+const loginGoogle = async ( req, res = response ) => {
+
+    try {
+        const { email, name, picture } = await googleVerify( req.body.token );   
+        
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if ( !usuarioDB ) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        await usuario.save();
+
+        const token = await generarJWT(usuarioDB.id);
+
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })        
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    loginGoogle
 }
